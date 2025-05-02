2dfaeaafc5dc1a8a5ab758d7cce199a98a490ece
IAmSir1 <ajmanriquez@miuandes.cl>
2025-05-01 20:10:23 -0400

    lab 7 creo que completo

--- whats_new/app/models/chat.rb
+++ whats_new/app/models/chat.rb
@@ -1,3 +1,17 @@
 class Chat < ApplicationRecord
+    belongs_to :sender, class_name: 'User'
+    belongs_to :receiver, class_name: 'User'
+    has_many :messages
 
+    validates :sender_id, presence: true
+    validates :receiver_id, presence: true
+    validate :different_users
+  
+    private
+  
+    def different_users
+      if sender_id == receiver_id
+        errors.add(:sender_id, "Users must be different")
+      end
+    end
 end
